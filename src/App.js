4571470cca8efcbe066786f75e3a4171198f1d90
LeeMinyeon <alsdus1412@gmail.com>
2021-03-26 09:48:56 +0900

    0325

--- src/App.js
+++ src/App.js
@@ -1,68 +1,54 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-
-function Food({ name , pic, rating}){
-  return <div>
-    <h1>I like {name}!</h1>
-    <h4>{rating}/5.0</h4>
-    <img src={pic} alt={name}/>
-  </div>
-}
-
-const foodILike = [
-  {
-    id:1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-      rating:5
-  },
-  {
-    id:2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating:4.9
-  },
-  {
-    id:3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating:4.8
-  },
-  {
-    id:4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating:5.5
-  },
-  {
-    id:5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-      rating:4.7
+class App extends React.Component{
+  //변하고 싶은 값을 넣는곳
+  state={
+    isLoading:true,
+    movies:[]
+  };
+  getMovies=async()=>{
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   }
-];
-
-
-Food.propTypes={
-  //props의 값이 스트링인지 넘버인지 확인할 수있다.그이외의 불린, 오브젝트등등확인가능
-  //propTypes의 이름은 반드시 propTypes로 지어야한다 아니면 react가 이해할수 없다.
-  name:PropTypes.string.isRequired,
-  pic:PropTypes.string.isRequired,
-  rating:PropTypes.number.isRequired
-}
-function App() {
-
-  return (
-    <div>
-        
-        {foodILike.map(dish=> <Food key={dish.id }name={dish.name} pic={dish.image} rating={dish.rating}/>)}
-    </div>)
+  componentDidMount(){
+    this.getMovies();
+  }
+  render(){
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading
+          ? (<div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>)
+          : (<div className="movies">
+            {
+              movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+             
+               ))}
+          </div>
+          )}
+        </section>
+      );
+  }
 }
 
 export default App;
